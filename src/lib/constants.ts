75046c8f4bc494bc6501662dcbe022231873ba1b
Chuck Hriczko <chriczko@libertyfoxtech.com>
2023-01-27 20:05:07 -0500

    Updated WebSocket URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 const Constants = {
-    WEBSOCKET_URL: 'ws://localhost:3031/chat'
+    //WEBSOCKET_URL: 'ws://localhost:3031/chat'
+    WEBSOCKET_URL: 'wss://ws.thedevelopingweb.com/chat'
 };
 
 export default Constants;
